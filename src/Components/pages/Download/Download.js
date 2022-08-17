ecba37df8021db70a078f7a5875ab59aef0e5345
Shivkumar54 <mshivkumar59@gmail.com>
2022-08-17 19:38:01 +0530

    completed wynk

--- src/Components/pages/Download/Download.js
+++ src/Components/pages/Download/Download.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import DownloadImage from "../../images/download.svg"
+import "./download.css"
 const Download = () => {
   return (
-    <div>
-      <h1>Updating Download Soon.....</h1>
+    <div className='downloader container '>
+      <div className="imagefile">
+        <img src={DownloadImage} alt="" width="350px"/>
+      </div>
+      <div className="contact">
+        <h2>Take Wynk wherever you go</h2>
+        <p>Get a message to download the mobile app</p>
+
+        <input type="text" placeholder='+91' className='static' />
+        <input type="text" placeholder='Mobile number' />
+        <br />
+        <button type="submit">Get the Link</button>
+      </div>
     </div>
   )
 }
--- src/Components/pages/SignUp/SignUp.js
+++ src/Components/pages/SignUp/SignUp.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import SignUpImage from "../../images/signup.svg"
 
 const SignUp = () => {
   return (
-    <div>
-            <h1>Updating SignUp Soon.....</h1>
+    <div className='downloader container '>
+      <div className="imagefile">
+        <img src={SignUpImage} alt="" width="350px"/>
+      </div>
+      <div className="contact">
+        <h2>Login/Sign Up</h2>
+        <p>Get a personalised experience, and access all your music</p>
+
+        <input type="text" placeholder='+91' className='static' />
+        <input type="text" placeholder='Mobile number' />
+        <br />
+        <button type="submit">Send OTP</button>
+        <div className="apper">
+          <img src="https://wynk.in/_next/static/media/play-store.28ce99c9.png?imwidth=256" alt="" />
+          <img src="https://wynk.in/_next/static/media/apple-store.fd317c9d.png?imwidth=256" alt="" />
+        </div>
+      </div>
     </div>
   )
 }
